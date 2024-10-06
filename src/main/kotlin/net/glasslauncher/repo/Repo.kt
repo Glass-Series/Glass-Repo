c9d01197b3caa1cd877928e354097aa5309da6e8
calmilamsy <bumbill00@gmail.com>
2024-10-06 20:18:32 +0100

    Fix crash when missing repo folders
    Make the other stuff use these in the future.

--- src/main/kotlin/net/glasslauncher/repo/Repo.kt
+++ src/main/kotlin/net/glasslauncher/repo/Repo.kt
@@ -6,8 +6,24 @@ import java.io.File
 class Repo {
 
     companion object RepoConsts {
-        val modRepoPath: File = File("repo/mods")
+        val modRepoPath = File("repo/mods")
+        val avatarsPath = File("repo/avatars")
+        val cdnPath = File("repo/cdn")
+        val classMapsPath = File("repo/classMaps")
+        val mcCachePath = File("repo/minecraftCache")
+        val mcMetaPath = File("repo/minecraftMeta")
+        val usersPath = File("users")
 
         val modRepo: ModRepo = ModRepo()
+
+        init {
+            modRepoPath.mkdirs()
+            avatarsPath.mkdirs()
+            cdnPath.mkdirs()
+            classMapsPath.mkdirs()
+            mcCachePath.mkdirs()
+            mcMetaPath.mkdirs()
+            usersPath.mkdirs()
+        }
     }
 }
