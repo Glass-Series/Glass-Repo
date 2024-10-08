bb1040b358bb219bf44c050d80db3f86a5c2bcb4
calmilamsy <bumbill00@gmail.com>
2024-10-08 15:01:36 +0100

    Dependency pass

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,9 +39,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
     implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
-    implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.129-kotlin-1.4.20")
     implementation("io.ktor:ktor-server-pebble-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-resources-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
@@ -50,27 +48,31 @@ dependencies {
     implementation("io.ktor:ktor-server-compression-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-sessions-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    // Why does this need to exist? Wtf?
-    implementation("io.ktor:ktor-server-double-receive:$ktorVersion")
+    implementation("io.ktor:ktor-server-double-receive:$ktorVersion") // Why does this need to exist? Wtf?
     implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("org.apache.ant:ant:1.10.13")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
+
+    // Kotlin extensions
+    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")
 
     // Http client
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-apache5:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 
-    // Kotlin extensions
-    implementation("io.arrow-kt:arrow-core:1.2.4")
-    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")
-
     // Simplify my life when doing emails.
-    implementation("org.apache.commons:commons-email:1.5")
+//    implementation("org.apache.commons:commons-email:1.5")
 
-    // JSON reading.
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
+    // For dropbox backups, when that gets added.
+//    implementation("com.dropbox.core:dropbox-core-sdk:7.0.0")
+
+    // For interfacing with modrinth, when I get around to that.
+//    implementation("dev.masecla:Modrinth4J:2.0.0")
 
     // Database encryption.
     implementation("com.macasaet.fernet:fernet-java8:1.5.0")
